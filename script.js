64efde5f3b2e2523dc0d5faccbce6df25033a552
RyUmarof <ryumarof@gmail.com>
2024-10-26 17:29:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//math method
+alert('Sizda hozir random sonlar chiqshni boshled💀')
+
+let a = Math.trunc(Math.random() * 100)
+
+alert(`sizda chiqqan random son: ${a}`)
+
+confirm('Biz random sonni 2ga bolmoqchimiz va kopaytirmoqchimiz rozimisiz')
+
+Math.trunc(
+         alert(` 
+         Random orqali chiqqan son ${a}edi
+          Biz Uni 2kopaytirdik ${a *2}  chiqdi
+         Biz uni 2ga boldik ${a / 2} chiqdi
+         shu sonni 3ga boldik ${a/2/3} chiqdi
+         `)
+)
+
+
